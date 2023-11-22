6d0a7164dfbd03e02c5562287959993c50327b77
CREESTL <creestl.main@yandex.ru>
2023-11-22 13:29:08 +0300

    Initial commit

---
+++ hf_qa.py
@@ -0,0 +1,42 @@
+"""
+
+Q&A over PDF document using HF Pipelines. No chat memory. No RAG.
+Questions NOT about the document may result in an unpredictable answer.
+
+"""
+
+from langchain.document_loaders import PyPDFLoader
+from transformers import pipeline
+
+
+# Create a pipeline specifically for Q&A
+model_id = "distilbert-base-cased-distilled-squad"
+pipe = pipeline("question-answering", model=model_id, max_new_tokens=10, device=0)
+
+# Parse PDF document and load its contents into string
+loader = PyPDFLoader("ritchie.pdf")
+data = loader.load()
+context = ""
+# Parse only 8 pages
+for el in data[:8]:
+    context += el.page_content
+
+print(context)
+
+questions = [
+    "When was Guy Ritchie born?",
+    "What are Guy Ritchie's most popular films?",
+    "What awards did Guy Ritchie get?",
+    "What was Ritchie's first award?",
+    "When was film Sherlock Holmes created?",
+    "Who did Charlie Hunnam play?",
+    "What is Wrath of Man?",
+    "How much kilograms are there in a ton?",
+    "How far can ducks fly?",
+]
+
+
+# Get the answer for each question
+for question in questions:
+    res = pipe(question=question, context=context)
+    print("\n\n\n" + res["answer"])
